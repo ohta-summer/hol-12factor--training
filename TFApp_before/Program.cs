68fa15fd3aa6a254ffa559385cba2c7214a9b9db
NatsumiOhta <n.ohta@ts.infocom.co.jp>
2025-02-17 21:34:17 +0900

    定義位置を変更

--- TFApp_before/Program.cs
+++ TFApp_before/Program.cs
@@ -3,10 +3,22 @@
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-builder.Services.AddDbContext<TFAppContext>(options =>
-    options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("TFAppContext")));
+// [演習による変更]HttpContext へのアクセスを登録する
+builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddDistributedMemoryCache();
+// 保存先をDBにする
+builder.Services.AddDbContext<TFAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TFAppContext")));
+
+// [演習による変更]メモリへのキャッシュはしない
+// builder.Services.AddDistributedMemoryCache();
+
+// [演習による変更]Azure Cache for Redis サービスにキャッシュする
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+    options.InstanceName = builder.Environment.EnvironmentName.ToLower();
+});
 
 // セッションの設定
 builder.Services.AddSession(options =>
@@ -14,6 +26,12 @@ builder.Services.AddSession(options =>
     options.IdleTimeout = TimeSpan.FromMinutes(5);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
+});
+
+// [演習による変更]HTTPクライアントを登録する
+builder.Services.AddHttpClient("weather", httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://fnappdf4khwh57ruqq.azurewebsites.net/");
 });
 
 var app = builder.Build();
