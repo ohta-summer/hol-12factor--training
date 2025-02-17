6755fbf4fc9a23b938d377b029270353ce8623e7
NatsumiOhta <n.ohta@ts.infocom.co.jp>
2025-02-17 22:53:14 +0900

    Add workflow to migration db

--- TFApp_before/Migrations/20250217134704_AddUserMessage.Designer.cs
+++ TFApp_before/Migrations/20250217134704_AddUserMessage.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TFApp.Data;
 
@@ -10,9 +11,11 @@ using TFApp.Data;
 namespace TFApp.Migrations
 {
     [DbContext(typeof(TFAppContext))]
-    partial class TFAppContextModelSnapshot : ModelSnapshot
+    [Migration("20250217134704_AddUserMessage")]
+    partial class AddUserMessage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- TFApp_before/Models/User.cs
+++ TFApp_before/Models/User.cs
@@ -5,4 +5,6 @@ public class User
     public string UserId { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
+    //[演習による変更] Migration
+    public string Message { get; set; } = string.Empty;
 }
