11f9274a1ec501ec68c1525c1a250502bc0fb27b
NatsumiOhta <n.ohta@ts.infocom.co.jp>
2025-02-17 21:45:06 +0900

    add using

--- TFApp_before/Program.cs
+++ TFApp_before/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.Extensions.Caching.StackExchangeRedis;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
